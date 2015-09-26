67037aca7c1d755c17c297b66179d21cd8d00477
Quentin Brault <tititesouris@laposte.net>
2015-09-26 11:02:21 +0200

    Hello World

---
+++ src/tournament/Main.java
@@ -0,0 +1,12 @@
+package tournament;
+
+/**
+ * Created by Quentin Brault on 26/09/2015.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+
+}
